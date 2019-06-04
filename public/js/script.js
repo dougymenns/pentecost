86ac88d3bb3ca49b8bf9758f929f35c313266d57
Captain <mcjohnsonlyndon@gmail.com>
2019-06-04 18:43:39 +0000

    Front end setup for website

---
+++ public/js/script.js
@@ -0,0 +1,22 @@
+// SideNav Button Initialization
+$(".button-collapse").sideNav();
+// SideNav Scrollbar Initialization
+var sideNavScrollbar = document.querySelector('.custom-scrollbar');
+Ps.initialize(sideNavScrollbar);
+
+$(function () {
+	$('.material-tooltip-main').tooltip({
+		template: '<div class="tooltip md-tooltip-main"><div class="tooltip-arrow md-arrow"></div><div class="tooltip-inner md-inner-main"></div></div>'
+	});
+})
+
+// MDB Lightbox Init
+$(function () {
+	$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+});
+
+// video modal
+$('#modal1').on('hidden.bs.modal', function (e) {
+	// do something...
+	$('#modal1 iframe').attr("src", $("#modal1 iframe").attr("src"));
+});
