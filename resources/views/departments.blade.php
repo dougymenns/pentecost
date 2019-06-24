a7ab625f64e37eef04e35953ef0ba0f189852282
Captain <mcjohnsonlyndon@gmail.com>
2019-06-24 23:18:17 +0000

    ampersand error fixed

--- resources/views/departments.blade.php
+++ resources/views/departments.blade.php
@@ -10,6 +10,7 @@
 			@foreach($departments as $department)
 				@php $department_id = str_replace(' ','-',$department->name);@endphp
 				@php $department_id = str_replace('\'','-',$department_id);@endphp
+				@php $department_id = str_replace('&','-',$department_id);@endphp
 				<div class="col-md-6" style="padding: 0px;">
 					<a class="collapse-trigger" data-toggle="collapse" href="#{{ $department_id }}" aria-expanded="false" aria-controls="{{ $department_id }}">
 						<div class="view overlay zoom" style="height: 30vh; width: 100%;">
