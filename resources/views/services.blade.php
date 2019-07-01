a6ee1a39a64fd4d6a189160e9a38f6d283de7020
Captain <mcjohnsonlyndon@gmail.com>
2019-07-01 23:09:55 +0000

    pause

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -1,36 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
+
 	<div class="container pt-4 p-4">
 		<h4 class="mt-3 text-center" style="font-family: 'Montserrat', sans-serif;">SERVICES AND EVENTS</h4>
 		<hr class="mb-3 hr" style="width: 80px; border: solid 0.5px black;">
-		@include('layouts.success')
-		@include('layouts.errors')
 		<div class="row">
 			@foreach($services as $service)
-				@php $truncated = Str::limit($service->description, 90); @endphp
 				<div class="col-md-4 text-center" style="padding: 10px !important;">
 					<div class="service-card p-4">
 						<h6 class="font-weight-bold text-uppercase">{{ $service->name }}</h6>
-						<p class="truncated" style="display: block !important;">{!! $truncated !!}<a class="collapse-trigger text-primary">read more</a></p>
-						<p class="full" style="display: none !important;">{!! $service->description !!}<a class="collapse-close text-primary">...read less</a></p>
+						<p class="full" style="display: none !important;">{!! $service->description !!}</p>
 						<p class="font-small font-weight-bold text-capitalize">Happens every {{ $service->recurrence }}</p>
 					</div>
 				</div>
 			@endforeach
 		</div>
 	</div>
-
-	<script>
-		$(".collapse-trigger").click(function() {
-			$(".truncated").style.display = "none !important";
-			$(".full").style.display = "block !important";
-		});
-
-		$(".collapse-close").click(function() {
-			$(".full").style.display = "none !important";
-			$(".truncated").style.display = "block !important";
-		})
-	</script>
 
 @endsection
