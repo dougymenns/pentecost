b0a82fb574eb17ef62763076a876993ac3434c88
Captain <mcjohnsonlyndon@gmail.com>
2019-07-01 23:18:59 +0000

    seeing

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -7,8 +7,8 @@
 		<hr class="mb-3 hr" style="width: 80px; border: solid 0.5px black;">
 		<div class="row">
 			@foreach($services as $service)
-				<div class="col-md-4 text-center" style="padding: 10px !important; height: 100% !important;">
-					<div class="service-card p-4">
+				<div class="col-md-4 text-center" style="padding: 10px !important;">
+					<div class="service-card p-4" style="height: 100% !important;">
 						<h6 class="font-weight-bold text-uppercase">{{ $service->name }}</h6>
 						<p class="full" style="display: none !important;">{!! $service->description !!}</p>
 						<p class="font-small font-weight-bold text-capitalize">Happens every {{ $service->recurrence }}</p>
